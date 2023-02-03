c0c5b6e86cbe78758fb5f164db526b7cee2391e7
fahAngelina <123092370+fahAngelina@users.noreply.github.com>
2023-02-03 11:33:47 +0000

    correctly

--- .ccls-cache/@home@runner@group-sorting-little-hope/sorting.h
+++ .ccls-cache/@home@runner@group-sorting-little-hope/sorting.h
@@ -1,38 +1,82 @@
 // selection sort function module in C
 
-void display(int a[],int n){
+void display(int a[], int n) {
 
-    int i;
+  int i;
 
-    for(i=0;i<n;i++)
-        printf("%5d",a[i]);
+  for (i = 0; i < n; i++)
+    printf("%5d", a[i]);
+  printf("\n");
+}
+
+void swap(int *a, int *b) {
+  int temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void bubbleSort(int a[], int n) {
+
+  int i, j;
+  int sorted;
+  // how may pair to compare?
+  for (i = 0; i < n - 1; i++) {
+    // printf("ROUND %d\n",i);
+    sorted = 0;
+    for (j = 0; j < n - 1 - i; j++) {
+      if (a[j] < a[j + 1]) {
+        swap(&a[j], &a[j + 1]);
+        sorted = 1;
+      }
+      display(a, n);
+    }
+    if (sorted == 0)
+      break;
+     printf("\n");
+  }
+}
+
+//ตั้งแต่ตรงนี้เป็นของงานวันนี้
+void selectionSort(int a[], int n) {
+  int i, j;
+
+  // int m = a[0];
+  int mi = 0; //ตัวเก็บค่าตน.น้อยสุด
+
+  for (j = 0; j < n - 1; j++) {
+    // m=a[j];
+    mi = j;
+
+    for (i = j + 1; i < n; i++) {
+      if (a[mi] < a[i]) {
+        // m = a[i];
+        mi = i;
+      }
+    }
+
+    swap(&a[j], &a[mi]);
+
+    // printf("Minimum is %d\n",m);
     printf("\n");
-  
+    display(a, n);
+  }
 }
 
+void insertionSort(int x[], int n) {
 
-
-void swap(int *a,int *b){
- int temp;
-  temp=*a;
-  *a=*b;
-  *b=temp;
-  
+  int j, i;
+  for (j = 1; j < n; j++) {
+    int value = x[j];
+    for (i = j - 1; i >= 0; i--) {
+      if (value < x[i])
+        break;
+      else
+        x[i + 1] = x[i];
+      display(x, n);
+    }
+    x[i + 1] = value;
+    display(x, n);
+    printf("\n");
+  }
 }
-
-void prebubbleSort(int a[],int n){
-
-int i,j;
-int sorted;
-// how may pair to compare?
-for(j=1;j<=n-1;j++){
- 
-  display(a,n);
- }
-  
-}
-
-
-
-
-
--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 
-#include "sorting.h"
+
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include "sorting.h"
 
 int main(int argc, char *argv[]) {
   int i, *a, n, j = 0;
@@ -10,9 +12,8 @@ int main(int argc, char *argv[]) {
   for (i = 0; i < n; i++) {
     a[i] = atoi(argv[i + 2]);
   }display(a, n);
-  if(strcmp(argv[2], "bubble")==0) bubbleSort(a, n);
-  else if(strcmp(argv[2], "insertion")==0) insertion(a,n);
-  else if(strcmp(argv[2], "selection")==0) selectionSort(a,n);
-  display(a, n);
+  if(strcmp(argv[1], "bubble")==0) bubbleSort(a, n);
+  else if(strcmp(argv[1], "insertion")==0) insertionSort(a,n);
+  else if(strcmp(argv[1], "selection")==0) selectionSort(a,n);
   return 0;
 }
