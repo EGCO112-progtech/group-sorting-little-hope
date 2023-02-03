08da5573b8505e6c3d39d636b9f2aac76132a572
CHUTIYA THANALUCK <122534225+mint-cty@users.noreply.github.com>
2023-02-03 10:28:32 +0000

    sorting

--- sorting.h
+++ sorting.h
@@ -28,7 +28,7 @@ for(i = 0; i<n-1; i++){
   //printf("ROUND %d\n",i);
   sorted = 0;
   for(j=0;j < n-1-i; j++){
- if(a[j] > a[j+1]){
+ if(a[j] < a[j+1]){
   swap(&a[j],&a[j+1]);
    sorted = 1;
    }
