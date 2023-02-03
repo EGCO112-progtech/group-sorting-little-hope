6f48d846f6181585cb4fbd78ce249bf87461e2be
fahAngelina <123092370+fahAngelina@users.noreply.github.com>
2023-02-03 10:28:21 +0000

    new main

--- .ccls-cache/@home@runner@group-sorting-little-hope/main.c
+++ .ccls-cache/@home@runner@group-sorting-little-hope/main.c
@@ -4,17 +4,14 @@
 
 int main(int argc, char *argv[]) {
   int i, *a, n, j = 0;
-  n = argc - 1;
+  n = argc - 2;
   a = (int *)malloc(sizeof(int) * n);
   for (i = 0; i < n; i++) {
-    a[i] = atoi(argv[i + 1]);
-  }
-
-  display(a, n);
-  bubbleSort(a, n);
-  // insertion(a,n);
-
-  // selectionSort(a,n);
+    a[i] = atoi(argv[i + 2]);
+  }display(a, n);
+  if(strcmp(argv[2], "bubble")==0) bubbleSort(a, n);
+  else if(strcmp(argv[2], "insertion")==0) insertion(a,n);
+  else if(strcmp(argv[2], "selection")==0) selectionSort(a,n);
   display(a, n);
   return 0;
 }
--- .ccls-cache/@home@runner@group-sorting-little-hope/sorting.h
+++ .ccls-cache/@home@runner@group-sorting-little-hope/sorting.h
@@ -10,21 +10,7 @@ void display(int a[],int n){
   
 }
 
-void selectionSort(int data[], int length) 
-{ 
-	int i, j, m, mi; 
-	
-     display(data,length);
-     
-	
-} 
 
-
-
-void insertion(int a[],int n){
-
-
-}
 
 void swap(int *a,int *b){
  int temp;
@@ -46,25 +32,7 @@ for(j=1;j<=n-1;j++){
   
 }
 
-void bubbleSort(int a[], int n) {
 
-  int i, j;
-  int sorted;
-  // how may pair to compare?
-  for (j = 0; j < n - 1; j++) {
-    // printf("round %d\n",j);
-    sorted = 0;
-    for (i = 0; i < n - 1 - j; i++) {
-      if (a[i] > a[i + 1]) {
-        swap(&a[i], &a[i + 1]);
-        sorted = 1;
-      }
-      display(a, n);
-    }
-    if (sorted == 0)
-      break;
-  }
-}
 
 
 
